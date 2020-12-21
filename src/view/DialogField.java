3ade6c5e8a0f254daeb43c23ba43e6f53b5ca20d
Starzkey <maxstarzyk96@gmail.com>
2020-12-21 03:06:07 +0100

    Dialogfeld

---
+++ src/view/DialogField.java
@@ -0,0 +1,40 @@
+package view;
+
+import javax.swing.*;
+
+public class DialogField extends JDialog {
+
+    //Attributes
+    JOptionPane jOptionPane;
+    JFrame frame;
+    String question;
+    String questionTitle;
+    Object[] options;
+    int playerAnswer;
+
+
+    public DialogField(JFrame frame, String question, String questionTitle, Object[] options) {
+
+        this.frame = frame;
+        this.questionTitle = questionTitle;
+        this.options = options;
+        this.question = question;
+        init();
+    }
+
+    public void init() {
+
+        JDialog d = new JDialog(frame, questionTitle);
+
+
+        jOptionPane = new JOptionPane();
+        d.add(jOptionPane);
+        playerAnswer = jOptionPane.showOptionDialog(frame, question, questionTitle, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
+
+    }
+
+    public int getPlayerAnswer() {
+        return playerAnswer;
+    }
+
+}
